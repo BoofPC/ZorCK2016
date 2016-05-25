6f4d1d11f8efbd9c81f922ca0d032eb2efa2f6ef
Helen <keith5387h@ckstudents.org>
2016-05-25 09:47:45 -0700

    added move and take

---
+++ src/Verbs/Move.java
@@ -0,0 +1,17 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keith5387h
+ */
+public class Move extends Verb{
+    public Move(){
+        super("Move", new String[] {"Go"}, new boolean[]{false, false, true});
+    }
+}
---
+++ src/Verbs/Take.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * @author keith5387h
+ */
+public class Take extends Verb{
+    public Take(){
+        super("Take", new String[]{"Get"}, new boolean[]{false, true, false});
+    }
+}
