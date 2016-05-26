815d8e6060112e6976ac8d5190fe3ecec551e001
Helen <keith5387h@ckstudents.org>
2016-05-26 09:40:44 -0700

    Added convenience object Portal for set movement options in Area

---
+++ src/core/Portal.java
@@ -0,0 +1,33 @@
+
+package core;
+
+/**
+ *
+ * @author keith5387h
+ */
+public class Portal {
+   
+    boolean locked;
+    String target; //Should be the name of an area to go to
+    
+    public Portal(boolean locked, String target){
+        this.locked = locked;
+        this.target = target;
+    }
+    
+    public boolean isLocked(){
+        return this.locked;
+    }
+    
+    public void lock(){
+        this.locked = true;
+    }
+    
+    public void unlock(){
+        this.locked = false;
+    }
+    
+    public String getTarget(){
+        return this.target;
+    }
+}
