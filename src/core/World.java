0a00015831dddd0c3fcf7649ed3d114a07dd8a58
unknown <mccay0421c@CK37643.cksd.wednet.edu>
2016-05-26 09:12:59 -0700

    Removed isAccessibility for implementation of portal

--- src/core/World.java
+++ src/core/World.java
@@ -9,19 +9,14 @@ package core;
  *
  * @author alexb
  */
+
+//remocing accessiblity, adding portals 
 public class World {
     private Area[][] map;
-    private boolean[][] isAccessibleNorth;
-    private boolean[][] isAccessibleSouth;
-    private boolean[][] isAccessibleEast;
-    private boolean[][] isAccessibleWest;
     
+
     public World(int size){
         this.map = new Area[size][size];
-        this.isAccessibleNorth = new boolean[size][size];
-        this.isAccessibleSouth = new boolean[size][size];
-        this.isAccessibleEast = new boolean[size][size];
-        this.isAccessibleWest = new boolean[size][size];
     }
     
     public void addArea(int x, int y, Area newArea){
@@ -30,19 +25,5 @@ public class World {
     
     public Area getAreaAt(int x, int y){
         return this.map[x][y];
-    }
-    
-    
-    public void setAccessibilityNorth(int x, int y, boolean isAccessible){
-        this.isAccessibleNorth[x][y] = isAccessible;
-    }
-    public void setAccessibilityEast(int x, int y, boolean isAccessible){
-        this.isAccessibleEast[x][y] = isAccessible;
-    }
-    public void setAccessibilitySouth(int x, int y, boolean isAccessible){
-        this.isAccessibleSouth[x][y] = isAccessible;
-    }
-    public void setAccessibilityWest(int x, int y, boolean isAccessible){
-        this.isAccessibleWest[x][y] = isAccessible;
     }
 }
