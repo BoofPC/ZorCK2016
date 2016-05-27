4d542e49758db28c8f6b1274922709d11eaf5658
Cody McCay <Zmark67@users.noreply.github.com>
2016-05-27 12:12:05 -0700

    Update Open.java

--- src/verbs/Open.java
+++ src/verbs/Open.java
@@ -1,14 +1,15 @@
+package verbs;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package verbs;
 
+import core.Verb;
 /**
  *
  * @author Cody
  */
 public class Open {
-    
+    super("open", new String[]{"unseal", "unlock"}, new boolean[]{true,false,true});
 }
