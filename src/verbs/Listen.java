ad08272c23e2e4af8cc75d9e0bb8aac3c8134754
Alex Johnson <Alex Johnson>
2016-05-28 21:36:34 -0700

    Added more verbs to the verbs package including the rest of the five senses

---
+++ src/verbs/Listen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verbs;
+import core.Verb;
+/**
+ *
+ * @author Alex Johnson
+ */
+public class Listen extends Verb {
+    
+    public Listen(){
+        super("listen",
+                new String[]{"hear","pay attention"},
+                new boolean[]{true, false, false});
+    }
+}
---
+++ src/verbs/Taste.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verbs;
+import core.Verb;
+/**
+ *
+ * @author Alex Johnson
+ */
+public class Taste extends Verb {
+    
+    public Taste(){
+        super("taste",
+                new String[]{"lick","eat","savor","sample","consume","devour"},
+                new boolean[]{false, true, false});
+    }
+}
