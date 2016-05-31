4d7e4ff6472bf1b040ceb5c232e65edfa02aa2ca
Peter Mattsen <peter.mattsen@gmail.com>
2016-05-31 11:06:36 -0700

    Updated Area1

--- src/areas/Test1.java
+++ src/areas/Test1.java
@@ -32,13 +32,5 @@ public class Test1 extends Area{
         public void interact(String command, Player player){
                 
         }
-    
-        public void enter(Player player){
-                
-        }
-    
-        public void exit(Player player){
-                
-        }
        
 }
