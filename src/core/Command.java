1a8f167bfa4afabeeab2c531c9f249930ce27bc2
Samantha Miller <sammiller1999@hotmail.com>
2016-06-01 15:04:52 -0700

    Reverted getter removal as per regular Java style.
    Getters are recommended on any data object (as seen in JavaBeans
    specification, 7.1).
    ~Brayden

--- src/core/Command.java
+++ src/core/Command.java
@@ -4,16 +4,27 @@ package core;
  *
  * @author pedro
  */
-public class Command {
+public final class Command {
     
-    public final Verb verb;
-    public final Item noun;
-    public final int direction;
+    private final Verb verb;
+    private final Item noun;
+    private final int direction;
     
     public Command(Verb verb, Item noun, int direction){
         this.verb = verb;
         this.noun = noun;
         this.direction = direction;
     }
-         
+
+    public Verb getVerb() {
+        return verb;
+    }
+
+    public Item getNoun() {
+        return noun;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
 }
--- src/core/Context.java
+++ src/core/Context.java
@@ -6,15 +6,27 @@ import java.util.HashMap;
  *
  * @author pedro
  */
-public class Context {
+public final class Context {
     
-    public final Player player;
-    public final World world;
-    public final HashMap<String, Boolean> state;
+    private final Player player;
+    private final World world;
+    private final HashMap<String, Boolean> state;
     
     public Context(Player player, World world){
         this.player = player;
         this.world = world;
         this.state = new HashMap<>();
-    }  
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public HashMap<String, Boolean> getState() {
+        return state;
+    }
 }
