a3ae69144c58c0a4ca41e86a4d199d0f0808c1ce
Samantha Miller <sammiller1999@hotmail.com>
2016-06-01 00:47:19 -0700

    added give and a needlessly-complex hello run method

--- src/verbs/Give.java
+++ src/verbs/Give.java
@@ -1,10 +1,8 @@
 package verbs;
-import core.Command;
-import core.PlayerConstruct;
-import core.Verb;
+import core.*;
 /**
  *
- * @author pedro
+ * @author pedro & Samantha
  */
 
 
@@ -13,7 +11,7 @@ import core.Verb;
 //The inputs "Give item to Booth" and "Give item" are syntactically identical
 //Because of the simplicity of our parser, you should only have one item
 //which can receive in any room at any time, or else the player cannot choose
-//whith item to give to
+//which item to give to
 
 
 
@@ -26,6 +24,29 @@ public class Give extends Verb {
     }
     
     public void run(Command command, PlayerConstruct construct){
+        Item noun = command.getNoun();
         
+        Player player = construct.getPlayer();
+        
+        if(player.hasMatching(noun)){
+            Item receiver;
+            receiver = null;
+            for(int i = 0; i < player.getCurrentArea().listItems().length; i++){
+                if(player.getCurrentArea().listItems()[i].getUsageKey(14) == 2){
+                    receiver = player.getCurrentArea().listItems()[i];
+                    break;
+                }
+            }
+            if(receiver != null){
+                receiver.receive(noun);
+                player.removeItem(noun);
+                System.out.println("You gave the " + noun.getName() + " to "
+                + receiver.getName());
+            }else{
+                System.out.println("I don't see who you'd want to give that to");
+            }
+        }else{
+            System.out.println("You don't even have a " + noun.getName());
+        }
     }
 }
