289a1dd1a8f60725f887c2b135fcbd40031b4591
Samantha Miller <sammiller1999@hotmail.com>
2016-06-01 19:13:23 -0700

    Made new class with global direction variables.

---
+++ src/core/Directions.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ *
+ * @author Samantha
+ */
+public class Directions {
+    public static int NORTH = 0;
+    public static int NORTH_EAST = 1;
+    public static int EAST = 2;
+    public static int SOUTH_EAST = 3;
+    public static int SOUTH = 4;
+    public static int SOUTH_WEST = 5;
+    public static int WEST = 6;
+    public static int NORTH_WEST = 7;
+    public static int UP = 8;
+    public static int DOWN = 9;
+}
