d6efb7d1d0a1610e955b3fbb04ec06ed7de00fe6
Alex Johnson <Alex Johnson>
2016-06-02 20:32:08 -0700

    Added Henrichson's Room and finished Fisher's Room

--- src/areas/FishersRoom.java
+++ src/areas/FishersRoom.java
@@ -11,7 +11,7 @@ public class FishersRoom extends Area {
         setInitialDescription("You are in Fisher's room as various papers on "
                 + "the downfall of the Ming Empire and essays as well as Silk "
                 + "Roads trading changed China while a Reggae-sounding song "
-                + "blares through the speakers.");
+                + "blares through the speakers. There is a lantern on the floor.");
         setDescription("This is Fisher's room, it smells vaguely of perspiration "
                 + "and the Spanish Inquisition.");
         setDark(false);
@@ -19,13 +19,14 @@ public class FishersRoom extends Area {
         setTaste("You taste good ol' oxygen");
         setSound("Blaring reggae music pounds through your ear drums");
         addItem(new Door(false, "Northern Door", new Key(), getPortals().north()));
+        addItem(new Lantern());
     }
 
     @Override
     public void interact(Command command, Context context) {
         Item noun;
         noun = command.getNoun();
-        if(command.getVerb().equals("look")){
+        if(command.getVerb().getTitle().equals("look")){
             System.out.println("You see scattered papers around the room, some"
                     + " saying 'You can get it if you really want' and others"
                     + " discussing the extent to which the Kyoto Protocol assisted"
@@ -33,10 +34,24 @@ public class FishersRoom extends Area {
                     + " Nothing seems to be of interest in this room.");
         }
         
-        if((command.getVerb().equals("get") || command.getVerb().equals("pick up")) && (command.getNoun().equals("paper") || command.getNoun().equals("papers"))){
+        if(command.getVerb().getTitle().equals("take") && (command.getNoun().equals("paper") || command.getNoun().equals("papers"))){
             System.out.println("You feel a burning sensation as your legs and arms feel as if they"
                     + " are on fire. A faint voice in the distance yells 'Plagarism!'. You immediately"
                     + " drop the papers due to fright.");
+        }
+        
+        if((command.getVerb().getTitle().equals("hello"))){
+            System.out.println("Fisher says, 'Bring me some coffee.'");
+        }
+        
+        if(command.getVerb().getTitle().equals("take") && command.getNoun().equals("lantern")){
+            if(hasMatching(new Lantern())){
+                System.out.println("You picked up the lantern!");
+                removeItem(new Lantern());
+            }
+            else{
+                System.out.println("There is no lantern in here...");
+            }
         }
         
         else{
---
+++ src/areas/HendrichsonsRoom.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areas;
+import core.*;
+import items.*;
+/**
+ *
+ * @author Alex Johnson
+ */
+public class HendrichsonsRoom extends Area{
+
+    public HendrichsonsRoom(World containingWorld) {
+        super(containingWorld);
+        getPortals().west(new Portal(Portal.State.UNLOCKED, "Hallway1"));
+        setInitialDescription("You are in Hendrichson's room. It contains lab benches "
+                + "with chemicals and physics projects. You see a balloon on the floor "
+                + "that looks as if it has been recently used.");
+        setDescription("You are in Hendrichson's room.");
+        setDark(false);
+        setSmell("It smells like sodium chloride.");
+        setTaste("It doesn't taste like anything.");
+        setSound("You don't hear anything. It is eerie...");
+        addItem(new Door(false, "Western Door", new Key(), getPortals().west()));
+    }
+
+    @Override
+    public void interact(Command command, Context context) {
+        Item noun;
+        noun = command.getNoun();
+        if(command.getVerb().getTitle().equals("look")){
+            System.out.println(getDescription());
+        }
+        
+        if(command.getVerb().getTitle().equals("take") && command.getNoun().equals("balloon")){
+            System.out.println("The saliva from the previous user of the balloon "
+                    + "acts as adhesive to keep the balloon on the ground.");
+        }
+        
+        if(command.getVerb().getTitle().equals("drink") && (command.getNoun().equals("chemical") || command.getNoun().equals("chemicals"))){
+            Player player = context.getPlayer();
+            System.out.println("Bad choice. You have died.");
+            player.setDeath(Game.Status.SUICIDE);
+        }
+        
+        else{
+           if(command.getNoun() !=  null)
+                noun.interact(command, context);
+            if(!context.getSkipGeneral())
+                command.getVerb().run(command, context);
+        }
+    }
+}
