6cd2336938429d2b7c1dfedcd9e19a079d0a6693
Cole Van Pelt <colevanpelt@gmail.com>
2016-06-07 09:30:58 -0700

    added put verb and appropriate plumbing

--- src/verbs/Put.java
+++ src/verbs/Put.java
@@ -10,6 +10,21 @@ public class Put extends Verb {
 
     @Override
     public void run(final Command command, final Context construct) {
-
+        final Item noun = command.getNoun();
+        final Player player = construct.getPlayer();
+        
+        if(player.hasMatching(noun)){
+            if(noun.usage().puttable() == Item.PUTTABLE){
+                player.removeItem(noun);
+                player.getCurrentArea().item(noun);
+                System.out.println("You put " + noun.name() + " in " + player.getCurrentArea());
+            }
+            else{
+                System.out.println("I'm sorry " + player.getName() + ", I can't allow you to do that");
+            }
+        }
+        else{
+            System.out.println("You don't have the " + noun.name());
+        }
     }
 }
