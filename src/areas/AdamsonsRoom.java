e2a3994554568bd8b52c3d977b4394b0c3527e90
SmartFolk <smartfolk280@gmail.com>
2016-06-08 10:36:33 -0700

    Update AdamsonsRoom.java

--- src/areas/AdamsonsRoom.java
+++ src/areas/AdamsonsRoom.java
@@ -10,7 +10,7 @@ public class AdamsonsRoom extends Area {
 
         this.portals().north(new Portal(false, Hallway10.class))
                 .west(new Portal(true,BroadcastingRoom.class));
-        this.title("Adamson's Room").description("----------").shortDescription("-----")
+        this.title("Adamson's Room").description("Banners hang from the ceiling proclaiming the acomplishments of Students. In the middle of the room lay 5 rows of highly advanced computers probably made by aliens. at the front of the room lie two massive movie screen sized moniters with a misterious golden key attached.").shortDescription("It's a Nerds paradise")
                 .item(new Door(false, "Northern Door", null, this.portals().north()))
                 .item(new Door(true, "Western Door", Key.Adamsons.class, this.portals().west()));
     }
