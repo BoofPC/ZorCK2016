8cd12e1a4fd8a58a41748cdee853e1ce9688efae
Ryan Allen <rallen111398@gmail.com>
2016-06-09 08:41:34 -0700

    removed booth from compsci room because he did not belong there

--- src/areas/CompSciRoom.java
+++ src/areas/CompSciRoom.java
@@ -15,23 +15,11 @@ public class CompSciRoom extends Area {
                         + " Powerschool is open on Booth's computer. You see your grade is an F."
                         + " There is a door to the east that leads to Hallway05 and"
                         + " a locked door to the north that leads to Mrs. Wachtman's room.")
-                .shortDescription("This is the CompSci Room, Room 317.").articleThe(true)
-                .item(new MrBooth());
+                .shortDescription("This is the CompSci Room, Room 317.").articleThe(true);
     }
 
     @Override
     public void interact(final Command command, final Context context) {
-        final Player player = context.getPlayer();
-        if (context.getPlayer().hasMatching(new Blackmail())) {
-            System.out.println(
-                    "You blackmail Booth into grading an assinment worth 100000000 points");
-            player.setDeath(Game.Status.WIN);
-        }
-        if (context.getPlayer().hasMatching(new Screwdriver())) {
-            System.out.println(
-                    "you tamper with booth's chair. He falls and leaves to get a new chair.");
-            this.removeItem(new MrBooth());
-        }
         super.interact(command, context);
     }
 }
