ca48af4347a006ed893534f80133be64e71b821d
SmartFolk <smartfolk280@gmail.com>
2016-06-09 09:47:27 -0700

    added descriptions

--- src/areas/Hallway01.java
+++ src/areas/Hallway01.java
@@ -14,7 +14,7 @@ public class Hallway01 extends Area {
             .west(new Portal(false, HendrichsonsRoom.class))
             .northwest(new Portal(false, BirkenfeldsRoom.class));
         this.title("Hallway")
-            .description("----------").shortDescription("-----").articleThe(true)
+            .description("The school hallway must have been designed my a manic depressive, either that or someone obsessed by grey. The teachers had done their bit, hanging posters in primary colours, yet the overall impression was of drabness. Lily longed to paint the doors rainbow, or better yet blow off the roof and replace it with a glass dome. She could see plants in pots reaching up for the sunlight and butterflies flitting in-between. North is Rosenquest's room to the North West is Birkinfeld's room and to the west is Henrichson's room. ").shortDescription("It's a drap hallway with a motivational poster saying you can do it cougs.  North is Rosenquest's room to the North West is Birkinfeld's room and to the west is Henrichson's room").articleThe(true)
 
             .item(new Door(false, "Northern Door", null, this.portals().north()))
             .item(new Door(false, "Western Door", null, this.portals().west()))
