48686d3f8411ad176fab9825271ae2e69065892b
Ryan Allen <rallen111398@gmail.com>
2016-06-13 10:15:52 -0700

    changed compsci room to have story b description

--- src/areas/CompSciRoom.java
+++ src/areas/CompSciRoom.java
@@ -11,7 +11,7 @@ public class CompSciRoom extends Area {
         this.title("CompSci Room")
                 //This is not a logical sentence structure
                 .description("This is the CompSci room, Room 317."
-                        + " Powerschool is open on Booth's computer. You see your grade is an F."
+                        + " On the wall, writen in blood, is 'WHY WOULDN'T HE JUST GRADE OUR ASSINMENTS'."
                         + " There is a door to the east that leads to Hallway05 and"
                         + " a locked door to the north that leads to Mrs. Wachtman's room.")
                 .shortDescription("This is the CompSci Room, Room 317.").articleThe(true);
