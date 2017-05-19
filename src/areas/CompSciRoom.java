ca6907ae04e447d9b51ced73af8b5b382710c58a
RiukaHeartfilia <brod1299@hotmail.com>
2017-05-19 10:28:23 -0700

    Changed Description for Comp Sci Room

--- src/areas/CompSciRoom.java
+++ src/areas/CompSciRoom.java
@@ -10,9 +10,11 @@ public class CompSciRoom extends Area<NoState> {
         super(containingWorld);
         this.portals().east(new Portal(Portal.State.UNLOCKED, Hallway05.class));
         this.title("Comp. Sci. Room").articleThe(true)
-                .description("The room is dark. Only the light shining through "
-                        + "cracks in the roof light the room.\n"
-                        + "Old computers lie on dust-covered tabletops.\n"
+                .description("The room is dark. Only the light in the \n"
+                        + "room comes from a flickering ceiling light.\n"
+                        + "Old, run down, computers rest peacefully on \n"
+                        + "dust covered tables throughout the room.\n"
+                        + "On the floor is a note covered in blood.\n"
                         + "To the east is a door leading to the hallway.")
                 .shortDescription("You are in the Comp. Sci. Lab.").item(new BloodyNote())
                 .item(new BrokenComputers());
