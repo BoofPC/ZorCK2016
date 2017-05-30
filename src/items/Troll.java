b4ad144bb2d1eee428c8d691295858637d5d8032
Samantha Miller <sammiller1999@hotmail.com>
2017-05-29 23:45:06 -0700

    Troll isn't anything special. It's just a placeholder item for people to look at.

--- src/items/Troll.java
+++ src/items/Troll.java
@@ -1,4 +1,18 @@
 package items;
 
-public class Troll {
+import core.Item;
+
+/* This item doesn't do anything. The only purpose it serves
+*  is to have its name printed when the North Room prints out
+*  the list of its contents. */
+public class Troll extends Item {
+
+    public Troll() {
+        super();
+
+        this.usage().take(Item.UNTAKABLE);
+        this.name("Hungry Troll")
+                .synonym("troll")
+                .examine("You can only see it's glowing yellow eyes.");
+    }
 }
